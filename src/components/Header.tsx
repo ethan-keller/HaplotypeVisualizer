d95a02ebdc6d797957a98964b0472f2f6d5a9e01
ethan-keller <e.keller@student.tudelft.nl>
2021-12-26 16:24:13 +0100

    Add header items

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { Container, Nav, Navbar, NavDropdown, NavLink } from 'react-bootstrap';
+import { Nav, Navbar } from 'react-bootstrap';
 
 interface HeaderProps {
   setShowAbout: (showAbout: boolean) => void;
@@ -7,18 +7,13 @@ interface HeaderProps {
 
 const Header: React.FC<HeaderProps> = (props) => {
   return (
-    // <>
-    //   <Navbar bg='light'>
-    //     <Navbar.Brand>Haplotype Visualizer</Navbar.Brand>
-    //     <NavLink onClick={() => props.setShowSettings(true)}>Settings</NavLink>
-    //     <NavLink onClick={() => props.setShowAbout(true)}>About</NavLink>
-    //   </Navbar>
-    // </>
     <Navbar bg='light'>
       <Navbar.Brand>Haplotype Visualizer</Navbar.Brand>
       <Nav>
-        <Nav.Link href='#home'>Home</Nav.Link>
-        <Nav.Link href='#link'>Link</Nav.Link>
+        {/* <Nav.Link href='#pheno'>Pheno</Nav.Link>
+        <Nav.Link href='#trio'>Trio</Nav.Link> */}
+        <Nav.Link href='#settings'>Settings</Nav.Link>
+        <Nav.Link href='#about'>About</Nav.Link>
       </Nav>
     </Navbar>
   );
