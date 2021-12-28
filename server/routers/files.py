1effec0a57718af7cca80b6ef4bf8e807b068d67
ethan-keller <e.keller@student.tudelft.nl>
2021-12-28 18:17:25 +0100

    Add endpoint for GET specific file

--- server/routers/files.py
+++ server/routers/files.py
@@ -1,3 +1,4 @@
+from os import stat
 from typing import List
 
 from fastapi import APIRouter, Query, status
@@ -10,24 +11,40 @@ from server_data.data import files
 
 router = APIRouter(prefix="/files", tags=["files"])
 
+responses = {status.HTTP_400_BAD_REQUEST: {"description": "Invalid file extension", "model": str}}
 
-@router.get(
-    "/", response_model=List[File], status_code=status.HTTP_200_OK, summary="Get information about needed files"
-)
+
+@router.get("/", response_model=File, summary="Get a specific needed file")
+def getFiles(index: int = Query(..., ge=0, lt=len(files))):
+    """
+    Get a specific needed files by index.
+
+    - **index**: Index of the file
+    """
+    return files[index]
+
+
+@router.get("/all", response_model=List[File], summary="Get all needed files")
 def getFiles():
-    '''
-    Get the list of needed files.
-    '''
+    """
+    Get the list of all needed files.
+    """
     return files
 
 
-@router.put("/update", status_code=status.HTTP_200_OK, summary="Update the file path for a specific file")
+@router.put(
+    "/update", responses=responses, summary="Update the file path for a specific needed file",
+)
 def updateFilePath(path: FilePath, index: int = Query(..., ge=0, lt=len(files))):
-    '''
+    """
     Update the file path for one of the needed files.
     
     - **path**: File path
     - **index**: Index of the file
-    '''
+    """
+    if path.suffix not in files[index].file_extensions:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail=f"{path.suffix} files are not accepted for this entry"
+        )
     files[index].path = path
-    print(files)
+
--- server/schemas/file.py
+++ server/schemas/file.py
@@ -2,14 +2,18 @@ from enum import Enum
 from typing import List, Optional
 from pydantic import BaseModel
 
+
 class UploadStatus(Enum):
-	NO_UPLOAD = 0
-	SUCCESSFUL_UPLOAD = 1
-	WARNING_UPLOAD = 2
+    NO_UPLOAD = 0
+    SUCCESSFUL_UPLOAD = 1
+    WARNING_UPLOAD = 2
+
 
 class File(BaseModel):
-	name: Optional[str] = None
-	description: str
-	status: UploadStatus
-	required: bool
-	file_extensions: List[str]
+    name: Optional[str] = None
+    path: Optional[str] = None
+    description: str
+    status: UploadStatus
+    required: bool
+    file_extensions: List[str]
+
