96dcefbf6eda342f1ff51315f4b2b0384a270ffa
ethan-keller <e.keller@student.tudelft.nl>
2021-12-29 21:49:45 +0100

    Add file index enum

--- server/schemas/file.py
+++ server/schemas/file.py
@@ -1,14 +1,18 @@
-from enum import Enum
+from enum import IntEnum
 from typing import List, Optional
 from pydantic import BaseModel
 from pydantic.types import FilePath
 
 
-class UploadStatus(Enum):
+class UploadStatus(IntEnum):
     NO_UPLOAD = 0
     SUCCESSFUL_UPLOAD = 1
     WARNING_UPLOAD = 2
 
+class FileIndex(IntEnum):
+    GFA = 0
+    PHENO = 1
+    GFF = 2
 
 class File(BaseModel):
     name: Optional[str] = None
