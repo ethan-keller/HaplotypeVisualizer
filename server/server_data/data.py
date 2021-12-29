8bdbde373a3f1e4473fa5591b79c93681017fc3e
ethan-keller <e.keller@student.tudelft.nl>
2021-12-29 21:50:14 +0100

    Refactor + add gfa data on server

--- server/server_data/data.py
+++ server/server_data/data.py
@@ -1,9 +1,16 @@
+from enum import Enum
 from typing import List
 
+from gfapy.gfa import Gfa
+
 from schemas.file import File, UploadStatus
+
+files_base_path = "C:\\Users\\ethan\\Documents\\TUDelft\\Honours Program\\HAPLOTYPE_VISUALISATION\\HaplotypeVisualizer\\server\\server_data\\"
 
 files: List[File] = [
     File(description="GFA file", status=UploadStatus.NO_UPLOAD, required=True, file_extensions=[".gfa"]),
     File(description="Phenotype table", status=UploadStatus.NO_UPLOAD, required=False, file_extensions=[".csv"]),
     File(description="GFF file", status=UploadStatus.NO_UPLOAD, required=False, file_extensions=[".gff"]),
 ]
+
+gfa: Gfa = None
