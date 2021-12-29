f29ce99c3a3ef20e2478dbe087e422822262793d
ethan-keller <e.keller@student.tudelft.nl>
2021-12-29 21:49:34 +0100

    Add file preparation logic

--- server/logic/files.py
+++ server/logic/files.py
@@ -1,8 +1,10 @@
+from os.path import isfile, splitext
 from typing import List
-from pydantic import FilePath
+
 from schemas.file import UploadStatus
 from server_data.data import files
-from os.path import isfile, splitext
+
+from logic.gfa import prepare_gfa
 
 
 def validate_file_extension(path: str, accepted_extensions: List[str]) -> bool:
@@ -12,15 +14,23 @@ def validate_file_extension(path: str, accepted_extensions: List[str]) -> bool:
     else:
         return True
 
+
 def are_required_files_uploaded() -> bool:
     for file in files:
         if file.required and file.status is not UploadStatus.SUCCESSFUL_UPLOAD:
             return False
     return True
 
+
 def file_exists(path: str) -> bool:
     return isfile(path)
+
 
 def get_extension(path: str) -> str:
     _, extension = splitext(path)
     return extension
+
+
+def prepare_files() -> None:
+    # TODO: add other file preparations
+    prepare_gfa()
