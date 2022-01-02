1af709fde809d4f6b3b0b75c516e841fe02189c5
ethan-keller <e.keller@student.tudelft.nl>
2022-01-02 21:37:27 +0100

    Add custom exception for pydantic conversion errors

---
+++ server/errors/PydanticConversionError.py
@@ -0,0 +1,18 @@
+class PydanticConversionError(Exception):
+    """Exception raised for errors when converting third party types to pydantic types.
+
+    Attributes:
+        from_type -- Third party type name
+        to_type -- Pydantic type name
+        reason -- explanation of the error
+        message -- error message
+    """
+
+    def __init__(
+        self, from_type: str, to_type: str, reason: str, message: str = "Could not convert thrid party type to Pydantic type"
+    ):
+        self.from_type = from_type
+        self.to_type = to_type
+        self.reason = reason
+        self.message = f"Could not convert third party type: {from_type} to Pydantic type: {to_type}\nReason: {reason}"
+        super().__init__(self.message)
