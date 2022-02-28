7d103d122960c67af0ecf6dbe4e4f3e8230c86f6
ethan-keller <e.keller@student.tudelft.nl>
2022-02-28 15:53:56 +0100

    Add layout router

--- server/main.py
+++ server/main.py
@@ -1,17 +1,17 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from routers import files, gfa
+from routers import files, gfa, layout
 
 origins = ["http://localhost:3000"]
 
 server = FastAPI()
 server.include_router(files.router)
 server.include_router(gfa.router)
+server.include_router(layout.router)
 server.add_middleware(
     CORSMiddleware, allow_origins=origins, allow_credentials=True, allow_methods=["*"], allow_headers=["*"],
 )
-
 
 @server.get("/")
 def root():
