65e08a13f131f2b77c6d62a7944ed26c0fa79bdf
ethan-keller <e.keller@student.tudelft.nl>
2022-03-19 16:56:26 +0100

    Center spinner content

--- src/components/SpinnerAnnotated.tsx
+++ src/components/SpinnerAnnotated.tsx
@@ -7,10 +7,12 @@ interface SpinnerAnnotatedProps {
 const SpinnerAnnotated: React.FC<SpinnerAnnotatedProps> = (props) => {
   return (
     <>
-      <Card className='text-center' style={{width: '100%'}}>
-        <Card.Body>
-          <Card.Text>{props.message}</Card.Text>
-          <Spinner animation='border' />
+      <Card className='text-center' style={{ width: '100%', height: '100%' }}>
+        <Card.Body style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <Card.Body>
+            <Card.Text>{props.message}</Card.Text>
+            <Spinner animation='border' />
+          </Card.Body>
         </Card.Body>
       </Card>
       <br />
