d4fcbf5177b42a8e3e1abf1f452b477d7a4b9947
ethan-keller <e.keller@student.tudelft.nl>
2022-03-20 17:40:45 +0100

    Rename path edit modal

--- src/components/modals/EditPathsModal.tsx
+++ src/components/modals/EditPathsModal.tsx
@@ -1,15 +1,23 @@
+import { useEffect } from 'react';
 import { Button, Modal, Table, Form } from 'react-bootstrap';
 import gfaApi from '../../api/gfa';
-import { useAppSelector } from '../../store';
+import { setActivePaths, toggleActivePath } from '../../slices/graphSettings';
+import { useAppDispatch, useAppSelector } from '../../store';
 import SpinnerAnnotated from '../SpinnerAnnotated';
 
-interface EditColorsModalProps {
+interface EditPathsModalProps {
   onHide: () => void;
 }
 
-const EditColorsModal: React.FC<EditColorsModalProps> = (props) => {
+const EditPathsModal: React.FC<EditPathsModalProps> = (props) => {
   const { data: paths } = gfaApi.useGetPathsQuery();
+  const dispatch = useAppDispatch();
   const pathColors = useAppSelector((state) => state.graphSettings.pathColors);
+  const activePaths = useAppSelector((state) => state.graphSettings.activePaths);
+
+  useEffect(() => {
+    if (paths) dispatch(setActivePaths(paths.map((_) => true)));
+  }, [dispatch, paths]);
 
   return (
     <Modal onHide={props.onHide} show>
@@ -32,7 +40,11 @@ const EditColorsModal: React.FC<EditColorsModalProps> = (props) => {
                     <Button style={{ border: 0, backgroundColor: pathColors[path.index] }} />
                   </td>
                   <td className='text-end'>
-                    <Form.Check checked type='switch' />
+                    <Form.Check
+                      type='switch'
+                      checked={activePaths.length === 0 ? true : activePaths[path.index]}
+                      onChange={(e) => dispatch(toggleActivePath(i))}
+                    />
                   </td>
                 </tr>
               ))}
@@ -48,4 +60,4 @@ const EditColorsModal: React.FC<EditColorsModalProps> = (props) => {
   );
 };
 
-export default EditColorsModal;
+export default EditPathsModal;
