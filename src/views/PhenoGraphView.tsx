cbddf97332cec90afc5a14a649390bb3b7f04a59
ethan-keller <e.keller@student.tudelft.nl>
2022-04-22 20:23:13 +0200

    Add graphwrapper to phenograph view

--- src/views/PhenoGraphView.tsx
+++ src/views/PhenoGraphView.tsx
@@ -1,35 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import gfaApi from '../api/gfa';
-import layoutApi from '../api/layout';
-import Graph from '../components/graph/Graph';
-import GraphType from '../types/graph';
+import React from 'react';
 import Header from '../components/Header';
 import PhenoGraphSidebar from '../components/sidebar/PhenoGraphSidebar';
-import SpinnerAnnotated from '../components/SpinnerAnnotated';
-import { cytoscapeNodes, cytoscapeEdges } from '../cytoscape/cytoscape';
-import { useAppSelector } from '../store';
 import '../styles/pheno-graph-view.css';
+import GraphWrapper from '../components/graph/GraphWrapper';
 
 interface PhenoGraphViewProps {}
 
 const PhenoGraphView: React.FC<PhenoGraphViewProps> = (props) => {
-  const { data: layout, isLoading } = layoutApi.useGetNodePositionsQuery();
-  const { data: segments } = gfaApi.useGetSegmentsQuery();
-  const { data: links } = gfaApi.useGetLinksQuery();
-  const graphSettings = useAppSelector((state) => state.graphSettings);
-
-  const [graph, setGraph] = useState<GraphType | undefined>(undefined);
-
-  useEffect(() => {
-    if (segments && links) {
-      const graph = {
-        nodes: cytoscapeNodes(segments, graphSettings),
-        edges: cytoscapeEdges(links, graphSettings),
-      };
-      setGraph(graph);
-    }
-  }, [segments, links, graphSettings]);
-
   return (
     <>
       <Header />
@@ -39,11 +16,7 @@ const PhenoGraphView: React.FC<PhenoGraphViewProps> = (props) => {
         </div>
 
         <div className='ph-g-graph'>
-          {isLoading ? (
-            <SpinnerAnnotated message='Loading graph' />
-          ) : layout && graph && graphSettings ? (
-            <Graph graph={graph} settings={graphSettings} layout={layout} />
-          ) : null}
+          <GraphWrapper />
         </div>
 
         <div className='ph-g-navigator' style={{ backgroundColor: '#dbebb7' }}>
