846488909118df8ce8c60f9b5c3bc7180708b9ba
ethan-keller <e.keller@student.tudelft.nl>
2022-06-04 01:56:16 +0200

    Fix custom port CORS bug

--- src/haplovis/server/main.py
+++ src/haplovis/server/main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from haplovis.server.routers import files, gfa, layout, pheno, bookmarks
 
-origins = ["http://localhost:3000"]
 VERSION = "0.1.0"
 
 server = FastAPI()
@@ -13,7 +12,7 @@ server.include_router(pheno.router)
 server.include_router(bookmarks.router)
 server.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
