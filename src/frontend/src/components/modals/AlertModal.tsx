8a5cdb67517dc1e3ac58aaa1634cb7dff375cc43
ethan-keller <e.keller@student.tudelft.nl>
2022-06-06 14:59:40 +0200

    Add generic alert modal

---
+++ src/frontend/src/components/modals/AlertModal.tsx
@@ -0,0 +1,28 @@
+import { Button, Modal } from 'react-bootstrap';
+import { TiWarningOutline } from 'react-icons/ti';
+
+interface AlertModalProps {
+  title: string;
+  description: string;
+  secondaryDescription?: string;
+  onHide: () => void;
+  show: boolean;
+}
+
+const AlertModal: React.FC<AlertModalProps> = (props) => {
+  return (
+    <Modal onHide={props.onHide} show={props.show} backdrop='static' keyboard={false}>
+      <Modal.Header closeButton>
+        <TiWarningOutline size={28} color='orange' style={{ margin: 3 }} />
+        <Modal.Title>{props.title}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <p>{props.description}</p>
+        <p>{props.secondaryDescription}</p>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Body>
+    </Modal>
+  );
+};
+
+export default AlertModal;
