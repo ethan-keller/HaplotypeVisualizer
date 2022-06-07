f2b4dec6aa4ec17a62cf54a81f8496f60cef931f
ethan-keller <e.keller@student.tudelft.nl>
2022-06-08 00:52:36 +0200

    Fix initial graph loader bug

--- src/frontend/src/components/graph/Graph.tsx
+++ src/frontend/src/components/graph/Graph.tsx
@@ -48,7 +48,7 @@ const Graph: React.FC<GraphProps> = ({
         edges: cytoscapeEdges(filterLinks(links, layout, filter), paths, settings, pheno, isolate),
       } as GraphType;
     }
-  }, [paths, links, graphSettings, filter, pheno, isolate]);
+  }, [paths, segments, links, graphSettings, filter, pheno, isolate]);
 
   return graph ? (
     <CytoscapeWrapper graph={graph} layout={layout} pheno={pheno} />
